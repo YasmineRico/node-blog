156bd4cc6a5340f529113ea64328b80d83fdc44d
YasmineRico <ricolyasmine@gmail.com>
2023-02-16 16:26:41 -0600

    move files, create new directories, add EJS templating

--- blog/index.js
+++ blog/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
-
+const path = require('path');
 const app = new express();
+
+// EJS Template engine
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
 app.listen(4000, ()=>{
     console.log('App listening on port 4000')
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/index.html'))
+})
+
+app.get('/contact', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/contact.html'))
+})
+app.get('/about', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/about.html'))
+})
+
+app.get('/post', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/post.html'))
 })
